49ae42ec09ff94351ffc19fb9bccab11b3839d86
Igor Oliveira <igoroliveira9827@gmail.com>
2024-09-17 22:56:56 -0300

    feat: add constructor for user entity

--- PayFlow.Domain/Entities/UserEntity.cs
+++ PayFlow.Domain/Entities/UserEntity.cs
@@ -4,6 +4,17 @@ namespace PayFlow.Domain.Entities;
 
 public class UserEntity : BaseEntity
 {
+    public UserEntity(
+        string username, 
+        string email, 
+        string password
+    )
+    {
+        Username = username;
+        Email = email;
+        Password = password;
+    }
+
     public required string Username { get; set; }
     public required string Email { get; set; }
     public required string Password { get; set; }
